53446148beb0390a64497a64253a70191fa44498
kamranhasan1 <117094635+kamranhasan1@users.noreply.github.com>
2022-12-02 00:07:52 +0530

    Add files via upload

---
+++ class11_Comments.py
@@ -0,0 +1,10 @@
+#this is also a comment
+
+print("Hardik\nkumawat")
+'''will give you the output
+
+
+        Hardik 
+        Kumawat '''
+
+#You can use the shortcut control+/ to turn an entire line into a comment!
